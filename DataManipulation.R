a5f4b92f97725b2f64283be12fe0e6260da82d38
KaiSaSci <kai.sattler@outlook.com>
2023-10-09 11:35:39 +0200

    uploading Rscript!?

---
+++ DataManipulation.R
@@ -0,0 +1,42 @@
+# Set your working directory to where the folder is saved on your computer
+setwd("~/Library/CloudStorage/OneDrive-Personal/PhD/R/Coding_Club/CC-3-DataManip")
+
+# Load the elongation data
+elongation <- read.csv("EmpetrumElongation.csv", header = TRUE)   
+
+# Check import and preview data
+head(elongation)   # first few observations
+str(elongation)    # types of variables
+
+# Let's get some information out of this object!
+elongation$Indiv   # prints out all the ID codes in the dataset
+length(unique(elongation$Indiv))   # returns the number of distinct shrubs in the data
+
+# Here's how we get the value in the second row and fifth column
+elongation[2,5]
+
+# Here's how we get all the info for row number 6
+elongation[6, ]
+
+# And of course you can mix it all together!
+elongation[6, ]$Indiv   # returns the value in the column Indiv for the sixth observation
+# (much easier calling columns by their names than figuring out where they are!)
+
+# Let's access the values for Individual number 603
+elongation[elongation$Indiv == 603, ]
+
+
+
+# Subsetting with one condition
+
+elongation[elongation$Zone < 4, ]    # returns only the data for zones 2-3
+elongation[elongation$Zone <= 4, ]   # returns only the data for zones 2-3-4
+
+
+# This is completely equivalent to the last statement
+elongation[!elongation$Zone >= 5, ]   # the ! means exclude
+
+
+# Subsetting with two conditions
+elongation[elongation$Zone == 2 | elongation$Zone == 7, ]    # returns only data for zones 2 and 7
+elongation[elongation$Zone == 2 & elongation$Indiv %in% c(300:400), ]    # returns data for shrubs in zone 2 whose ID numbers are between 300 and 400
